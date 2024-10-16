44657930c540be8c3922e2f1633104b4d886a9d0
Qudus Oshinaike <llahsmine@gmail.com>
2024-10-16 10:48:34 +0100

    Basic CRUD Operation

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { isDate } = require('lodash');
+const { isDateString } = require('class-validator');
 
 const app = express();
 app.use(express.json());
@@ -6,18 +8,18 @@ app.use(express.json());
 // TODO: Create a crude api for managing user todos
 const users = [
   {
-    firstname: "John",
-    lastname: "Doe",
-    email: "john.doe@example.com",
-    phone: "1234567890",
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'john.doe@example.com',
+    phone: '1234567890',
   },
   {
-    firstname: "Jane",
-    "lastname": "Doe",
-    "email": "jane.doe@example.com",
-    phone: "1234567890",
-  }
-]
+    firstname: 'Jane',
+    lastname: 'Doe',
+    email: 'jane.doe@example.com',
+    phone: '1234567890',
+  },
+];
 
 const todos = [
   {
@@ -25,11 +27,7 @@ const todos = [
     date: '15/10/24',
     title: 'my tuesday tasks',
     isDone: false,
-   tasks: [
-    'going to study',
-    'going to gym',
-    'going to mall'
-   ]
+    tasks: ['going to study', 'going to gym', 'going to mall'],
   },
   {
     id: 3,
@@ -39,42 +37,61 @@ const todos = [
     tasks: [
       'make week plan',
       "going to grandma's place",
-      "going to beach",
-      "going to usrah"
-    ]
-  }
-]
+      'going to beach',
+      'going to usrah',
+    ],
+  },
+];
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!',);
+  res.send('Hello World!');
 });
 
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
   res.status(200).send(users);
-})
+});
 
-app.get("/todos", (req, res) => {
+app.get('/todos', (req, res) => {
   res.status(200).send(todos);
-})
+});
 
-app.post("/todos", (req, res) => {
+app.post('/todos', (req, res) => {
   // create a new todo
- const newTodo = req.body
+  const newTodo = req.body;
   todos.push(newTodo);
   res.status(201).send(todos);
-})
+});
 
-app.get("/todos/:id/:name", (req, res) => {
+app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
-  const todo = todos.find(todo => todo.id === parseInt(id));
+  const todo = todos.find((todo) => todo.id === parseInt(id));
   if (!todo) {
-    res.status(404).send("Todo not found");
+    res.status(404).send('Todo not found');
   }
   res.status(200).send(todo);
-})
+});
+app.put('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  let todo = todos.find((todo) => todo.id === parseInt(id));
+  if (!todo) {
+    res.status(404).send('Todo not found');
+  }
+  todo.date = req.body.date;
+  todo.title = req.body.title;
+  res.status(200).send(todo);
+});
 
+app.delete('/todo/:id', (req, res) => {
+  const id = req.params.id;
+  const todoIndex = todos.find((u) => u.id === parseInt(id));
+  if (todoIndex === -1) return res.status(404).send('todo not found');
+  const deletedTodo = todos.splice(todoIndex, 1);
+  res
+    .status(200)
+    .json({ message: 'successfully deleted the below', deletedTodo });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
